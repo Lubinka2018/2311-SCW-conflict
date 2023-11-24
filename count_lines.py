11fdf704f33dac7c0553d373c6a5b2bafe1922af
Marta Kargól @laptop <marta.kargol83@gmail.com>
2023-11-24 11:35:54 +0100

    Add explanation at the top
    I think  it is good  practice to document a script.

--- count_lines.py
+++ count_lines.py
@@ -1,6 +1,12 @@
 import.sys
 
+"""
+This script counts the lines in standard input
+Input:  text from the system
+""""
+
+
 count = 0
 for line in sys.stdin:
 	count +=1
-	print  ()
+print  (count, 'lines in standard input')
